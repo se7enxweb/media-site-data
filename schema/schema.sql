7e1d52e5dd7d818067f891cfa587ae547a9c6585
Edi Modrić <edi.modric@gmail.com>
2025-12-10 14:28:35 +0100

    Update field type identifiers to Ibexa 5 variants

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.4.6, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.4.7, for Linux (x86_64)
 --
 -- Host: localhost    Database: ngmedia
 -- ------------------------------------------------------
--- Server version	8.4.6
+-- Server version	8.4.7
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -1793,4 +1793,4 @@ CREATE TABLE `sckenhancedselection` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-11-24 12:00:00
+-- Dump completed on 2025-12-10 12:00:00
